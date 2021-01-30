93cf07449a97ca1736e6f62b5c448f81b1efdc59
Milana Birulia <milanabirulya@yandex.ru>
2021-01-31 00:23:20 +0300

    debug: править html разметку, css правила для блока popup, js функцию открытия и закрытия попап

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,55 @@
 // Находим форму элементы DOM
 let formElement = document.querySelector('.popup__form');
+// Находим из формы значения полей
 let nameInput = formElement.querySelector('#name');
-let jobInput = formElement.querySelector('#about-me');
+let jobInput = formElement.querySelector('#about');
+// Выбираем элементы, куда должны быть вставлены значения полей
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+// Находим попап, кнопки редактирования профиля и закрытия попапа
+let overlay = document.querySelector('.popup');
+let popupOpenButton = document.querySelector('.profile__button-edit');
+let popupCloseButton = overlay.querySelector('.popup__close');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+// функция обработчик «отправки» формы
 function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы (можно указывать как e или evt или event)
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    nameInput.value;
-    jobInput.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
-    // Вставьте новые значения с помощью textContent
+                                               
+    // Вставили новые значения полей с помощью textContent
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
+    // Вызываем функцию закрытия попапа
+    closePopup(evt);
 }
+
+//функция открытия попапа 
+function togglePopup() {
+  // Передаем данные из профеля в поля формы
+  nameInput.value = 'Жак-Ив Кусто';
+  jobInput.value = 'Исследователь океана';
+  // Добавляем класс (видимость попапа)
+  overlay.classList.toggle('popup_opened');
+}
+
+//функция закрытия попапа по оверлоу
+function closePopup(evt) {
+  if (evt.target === evt.currentTarget) {
+    //В момент закрытия модального окна полям передается пустое значение
+    nameInput.value = '';
+    jobInput.value = '';
+    togglePopup();
+  }
+} 
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
-let overlay = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.profile__button-edit');
-let popupCloseButton = overlay.querySelector('.popup__close');
-
-
-//функция открытия попапа 
-function togglePopup(evt) {
-  evt.preventDefault();
-  overlay.classList.toggle('popup_opened');
-}
-
-//функция закрытия попапа (по оверлоу)
-function closePopup(evt) {
-  if (evt.target === evt.currentTarget) {
-    togglePopup(evt)
-  }
-}
-
 //прикрепляем обработчик по клику на кнопку "Редактировать профиль" 
 popupOpenButton.addEventListener('click', togglePopup);
 
 //прикрепляем обработчик по клику на кнопку "Закрыть попап"
-popupCloseButton.addEventListener('click', togglePopup);
+popupCloseButton.addEventListener('click', closePopup);
 
 //прикрепляем обработчик по клику на оверлоу
-overlay.addEventListener('click', closePopup);
+overlay.addEventListener('mousedown', closePopup);
